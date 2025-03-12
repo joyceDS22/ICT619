6d12d6c05e7d3a24a36e9309d9fa7dbb40064f40
Joyce <99851140+joyceDS22@users.noreply.github.com>
2025-03-12 09:18:15 +0800

    Update Final Model (MLP+LR).py

--- Final Model (MLP+LR).py
+++ Final Model (MLP+LR).py
@@ -1,3 +1,11 @@
+%pip install scikit-learn
+%pip install imbalanced-learn
+%pip install numpy
+%pip install matplotlib
+%pip install seaborn
+%pip install pandas
+%pip install torch
+
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
